279d4662bc337e073a84ce59dedaffd972ae7a15
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-10-27 15:13:13 +0800

    Update add.php
    Provided documentation for Add PHP Script

--- admin/add.php
+++ admin/add.php
@@ -1,33 +1,30 @@
 <?php
+	// DO NOT MODIFY (START)
 	session_start();
 	include_once('connection.php');
+	// DO NOT MODIFY (END)
 
+	// RUN upon SUBMISSION
 	if(isset($_POST['add'])){
+		// VARIABLES for QUERY
 		$reference_number = $_POST['reference_number'];
 		$firstname = $_POST['firstname'];
 		$residence = $_POST['residence'];
 		$programme = $_POST['programme'];
+
+		// QUERY for ADD Operation
 		$sql = "INSERT INTO students (reference_number, firstname, residence, programme) VALUES ('$reference_number', '$firstname', '$residence', '$programme')";
 
-		//use for MySQLi OOP
+		// Provide accessible MESSAGES for operation result
 		if($conn->query($sql)){
 			$_SESSION['success'] = 'Member added successfully';
-		}
-		///////////////
-
-		//use for MySQLi Procedural
-		// if(mysqli_query($conn, $sql)){
-		// 	$_SESSION['success'] = 'Member added successfully';
-		// }
-		//////////////
-		
-		else{
+		} else{
 			$_SESSION['error'] = 'Something went wrong while adding';
 		}
-	}
-	else{
+	} else{
 		$_SESSION['error'] = 'Fill up add form first';
 	}
 
+	//REDIRECT to STUDENT PAGE upon SUBMISSION
 	header('location: 1index.php');
 ?>
