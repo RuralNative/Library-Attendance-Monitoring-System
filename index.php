83f141241b9e8d70e77b77e50e4d817a638b8c06
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-10-27 11:40:54 +0800

    Update index.php
    Provided format modifications for the code

--- index.php
+++ index.php
@@ -85,27 +85,27 @@
       }, 100);
 
       $('#attendance').submit(function(e){
-  e.preventDefault();
-  var attendance = $(this).serialize();
-  $.ajax({
-    type: 'POST',
-    url: 'attendance.php',
-    data: attendance,
-    dataType: 'json',
-    success: function(response){
-      if(response.error){
-        $('.alert').hide();
-        $('.alert-danger').show();
-        $('.message').html(response.message);
-      } 
-      else{
-        $('.alert').hide();
-        $('.alert-success').show();
-        $('.message').html(response.message);
-      }
-    }
-  });
-}); 
+        e.preventDefault();
+        var attendance = $(this).serialize();
+        $.ajax({
+          type: 'POST',
+          url: 'attendance.php',
+          data: attendance,
+          dataType: 'json',
+          success: function(response){
+            if(response.error){
+              $('.alert').hide();
+              $('.alert-danger').show();
+              $('.message').html(response.message);
+            } 
+            else{
+              $('.alert').hide();
+              $('.alert-success').show();
+              $('.message').html(response.message);
+            }
+          }
+        });
+      }); 
     });
   </script>
   <!-- DO NOT MODIFY (END) -->
