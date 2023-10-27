033b343ddb608a23aea7bbaea6f7d7b644c4e01a
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-10-27 15:12:56 +0800

    Update add_modal.php
    Provided documentation for Add Modal PHP file

--- admin/add_modal.php
+++ admin/add_modal.php
@@ -11,20 +11,21 @@
             </div>
             <div class="modal-body">
 				<div class="container-fluid">
+					<!-- ADD FORM (START) -->
 					<form method="POST" action="add.php">
 						<!-- LRN Input COMPONENT -->
 						<div class="row form-group">
 							<div class="col-sm-2">
-								<label class="control-label modal-label">Student ID:</label>
+								<label class="control-label modal-label">ID No.: *</label>
 							</div>
 							<div class="col-sm-10">
 								<input type="text" class="form-control" name="reference_number" required>
 							</div>
 						</div>
-						<!-- Full Name Input COMPONENT -->
+						<!-- Name Input COMPONENT -->
 						<div class="row form-group">
 							<div class="col-sm-2">
-								<label class="control-label modal-label">Full Name:</label>
+								<label class="control-label modal-label">Name: *</label>
 							</div>
 							<div class="col-sm-10">
 								<input type="text" class="form-control" name="firstname" required>
@@ -33,7 +34,7 @@
 						<!-- Residence Input COMPONENT -->
 						<div class="row form-group">
 							<div class="col-sm-2">
-								<label class="control-label modal-label">Address:</label>
+								<label class="control-label modal-label">City: *</label>
 							</div>
 							<div class="col-sm-10">
 								<input type="text" class="form-control" name="residence" required>
@@ -42,17 +43,23 @@
 						<!-- Programme Input COMPONENT -->
 						<div class="row form-group">
 							<div class="col-sm-2">
-								<label class="control-label modal-label">Programme:</label>
+								<label class="control-label modal-label">Course: *</label>
 							</div>
 							<div class="col-sm-10">
 								<input type="text" class="form-control" name="programme" required>
 							</div>
 						</div>
+						<!-- Cancel/Save BUTTONS -->
         				<div class="modal-footer">
-            				<button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-            				<button type="submit" name="add" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Save</a>
+            				<button type="button" class="btn btn-default" data-dismiss="modal">
+								<span class="glyphicon glyphicon-remove"></span> Cancel
+							</button>
+            				<button type="submit" name="add" class="btn btn-primary">
+								<span class="glyphicon glyphicon-floppy-disk"></span> Save</a>
+							</button>
 						</div>
 					</form>
+					<!-- ADD FORM (END) -->
 				</div>
     		</div>
 		</div>
