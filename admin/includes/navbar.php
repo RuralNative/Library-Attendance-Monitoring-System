34f742b8bddd5330ca41b318e1631b5310268ffe
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-01 12:17:08 +0800

    Update navbar.php

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,10 +1,12 @@
 <header class="main-header">
-    <!-- Logo -->
+    <!-- PAGE TITLE (LOGO) -->
     <a href="home.php" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>CPSU</b>Library</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>CPSU</b> LIBRARY</span>
+      <span class="logo-mini">
+        <b>CPSU</b>Library
+      </span>
+      <span class="logo-lg">
+        <b>CPSU</b> LIBRARY
+      </span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
