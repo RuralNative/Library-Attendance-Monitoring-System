b0de725b51338f639ed94236c92555fd1ddd587f
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-01 11:53:17 +0800

    Formatting for Footer and Header Scripts

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,6 +1,15 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Brought To You By <a href="https://www.youtube.com/channel/UCOBGHzFkIEf2laDLB1G8TrQ" target="_blank">Charlie E. Pelingon</a> | <a href="https://www.linkedin.com/in/johnberlinleonor/" target="_blank">John Berlin Leonor</a></b>
+      <b>
+        Developed by 
+        <a href="https://www.youtube.com/channel/UCOBGHzFkIEf2laDLB1G8TrQ" target="_blank">
+          Charlie Pelingon
+        </a> 
+        | 
+        <a href="https://www.linkedin.com/in/johnberlinleonor/" target="_blank">
+          John Berlin Leonor
+        </a>
+      </b>
     </div>
     <strong>Copyright &copy; 2023  |  CPSU - Moises Padilla Library Attendance System </strong>
 </footer>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -3,7 +3,8 @@
 <head>
   	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<title>SIA Library Attendance System</title>
+  	<!-- Tab Title Description -->
+    <title>CPSU - Moises Padilla Library Attendance System</title>
   	<!-- Tell the browser to be responsive to screen width -->
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   	<!-- Bootstrap 3.3.7 -->
