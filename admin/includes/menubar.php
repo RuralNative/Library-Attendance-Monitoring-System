91e5818f1b0adf93511967b5608e75e96ae44605
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-01 12:07:20 +0800

    Documentaion and Formatting for Menubar PHP

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -1,26 +1,44 @@
-<aside class="main-sidebar">
+<aside class="main-sidebar"> 
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-      <!-- Sidebar user panel -->
+      <!-- SIDEBAR USER PANEL -->
       <div class="user-panel">
+        <!-- USER PANEL Logo -->
         <div class="pull-left image">
           <img src='../images/cpsu_logo.webp' class="img-circle" alt="User Image">
         </div>
+        <!-- USER PANEL Title Description -->
         <div class="pull-left info">
           <p><b>CPSU - Moises Padilla</b></p>
           <p>Library</p>
-        
         </div>
       </div>
+
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
+        <!-- REPORTS Section -->
         <li class="header">REPORTS</li>
-        <li class=""><a href="home.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-        <li class="header">MANAGE</li>
-        
-        <li><a href="attendance.php"><i class="fa fa-calendar"></i> <span>Attendance</span></a></li>
-        <li><a href="1index.php"><i class="fa fa-users"></i> Students</a></li>
+        <li class="">
+          <a href="home.php">
+            <i class="fa fa-dashboard"></i> 
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <!-- DATA MANAGEMENT Section -->
+        <li class="header">DATA MANAGEMENT</li>  
+        <li>
+          <a href="attendance.php">
+            <i class="fa fa-calendar"></i> 
+            <span>Attendance</span>
+          </a>
+        </li>
+        <li>
+          <a href="1index.php">
+            <i class="fa fa-users"></i> 
+            Students
+          </a>
+        </li>
       </ul>
     </section>
     <!-- /.sidebar -->
-  </aside>
+</aside>
