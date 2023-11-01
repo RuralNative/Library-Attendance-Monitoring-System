d7d8625ebb56cb100dfb190b7ac47abada8194c3
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-01 11:46:28 +0800

    Documentation for Admin CON PHP
    Provide documentation and formatting for the PHP Script

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,8 +1,16 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'attendance');
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
+	//SQL Variables for Database Connection
+	$hostname = 'localhost';
+	$username = 'root';
+	$password = '';
+	$database = 'attendance';
 	
+	//Function for Database Connection
+	$conn = new mysqli($hostname, $username, $password, $database);
+
+	//Error Message upon Database Connection Failure
+	if ($conn->connect_error) {
+	    //Kill Script and Show Error Message
+		die("Connection failed: " . $conn->connect_error);
+	}	
 ?>
