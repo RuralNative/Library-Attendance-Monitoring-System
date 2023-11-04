8e18e15dece42b51b28ee6939ea40505be58734b
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-04 19:22:36 +0800

    Documentation for Connection PHP Script

--- conn.php
+++ conn.php
@@ -1,8 +1,15 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'attendance');
+	//Variables for Database Connection
+	$hostname = 'localhost';
+	$user = 'root';
+	$password = '';
+	$database = 'attendance';
 
+	//Initialize Database Connection
+	$conn = new mysqli($hostname, $user, $password, $database);
+
+	//Handle DB Connection Error
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-	
 ?>
