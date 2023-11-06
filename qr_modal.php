43147c1174c9d90dde2e2c42c6104f7da63f73ee
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-06 14:37:38 +0800

    Fix for Scripts Conflicts Error

--- qr_modal.php
+++ qr_modal.php
@@ -1,7 +1,5 @@
 <html>
 <head>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script src="https://unpkg.com/html5-qrcode"></script>
 </head>
 <body>
--- scripts.php
+++ scripts.php
@@ -1,10 +1,20 @@
-<!-- jQuery 3 -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<!-- jQuery UI 1.12.1 -->
+<!-- jQuery 3 JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<!-- jQuery UI 1.12.1 JS-->
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<!-- jQuery UI 1.12.1 CSS -->
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <!-- Bootstrap 4.3.1 JS -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <!-- Bootstrap 4.3.1 CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<!-- Font Awesome 5.15.3 CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<!-- Google Font CCS -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css"/>
 <!-- Moment JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+
+
+
