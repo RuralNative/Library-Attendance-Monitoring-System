8058e8a6c4dd621ef4da98ddad398fe67eb08454
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-06 14:32:48 +0800

    Modified Script Important
    For conflict resolution

--- scripts.php
+++ scripts.php
@@ -2,7 +2,9 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <!-- jQuery UI 1.12.1 -->
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<!-- Bootstrap 4.3.1 -->
+<!-- Bootstrap 4.3.1 JS -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- Bootstrap 4.3.1 CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <!-- Moment JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
