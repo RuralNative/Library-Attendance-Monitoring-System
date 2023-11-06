cbbd82796c1342016927ba06df7022cbbf94d930
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-06 11:07:11 +0800

    Fix for QR Reader Component Not Found Error
    Modified the code by relocating the HTML code block above the JS script so that the JS script can properly identify the component it is to utilize

--- qr_modal.php
+++ qr_modal.php
@@ -1,9 +1,29 @@
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
+<!-- MODAL -->
+<div class="modal fade" id="qrModal" tabindex="-1" role="dialog" aria-labelledby="qrModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="qrModalLabel">QR Scanner</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <!-- Div to show the QR code scanner -->
+                <div id="qr-reader" style="width:100%"></div>
+                <div id="qr-reader-results"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- JS Script for QR Input Handling -->
-(function($) {
 <script>
+(function($) {
     var resultContainer = document.getElementById('qr-reader-results');
     var lastResult, countResults = 0;
 
@@ -35,22 +55,3 @@
     });
 })(jQuery.noConflict(true));
 </script>
-
-<!-- MODAL -->
-<div class="modal fade" id="qrModal" tabindex="-1" role="dialog" aria-labelledby="qrModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="qrModalLabel">QR Scanner</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Div to show the QR code scanner -->
-                <div id="qr-reader" style="width:100%"></div>
-                <div id="qr-reader-results"></div>
-            </div>
-        </div>
-    </div>
-</div>
