c9248d4da3f724220aefe9221d036c08284a9d69
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-07 14:09:37 +0800

    Modified Installation Script
    Changed command to only copy it and include also its folders and content

--- installation.ps1
+++ installation.ps1
@@ -1,10 +1,11 @@
 $sourceFolder = "C:\xampp\htdocs\Library"
 $destinationFolder = "C:\Users\HP-PC\Documents\New folder"
 
-# Get all files in the source folder
-$files = Get-ChildItem -Path $sourceFolder -File
+# Copy files from the source folder to the destination folder
+Get-ChildItem -Path $sourceFolder -File | Copy-Item -Destination $destinationFolder
 
-# Move each file to the destination folder
-foreach ($file in $files) {
-    Move-Item -Path $file.FullName -Destination $destinationFolder
+# Copy folders and their contents from the source folder to the destination folder
+Get-ChildItem -Path $sourceFolder -Directory | ForEach-Object {
+    $destinationPath = Join-Path -Path $destinationFolder -ChildPath $_.Name
+    Copy-Item -Path $_.FullName -Destination $destinationPath -Recurse
 }
