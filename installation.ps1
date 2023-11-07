813eb67ff8ec3136f05d559bf4715cda14740e68
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-07 14:12:22 +0800

    Modified Installation Script Ver
    Modified the copy operation will be verbose in its operations

--- installation.ps1
+++ installation.ps1
@@ -2,10 +2,10 @@ $sourceFolder = "C:\xampp\htdocs\Library"
 $destinationFolder = "C:\Users\HP-PC\Documents\New folder"
 
 # Copy files from the source folder to the destination folder
-Get-ChildItem -Path $sourceFolder -File | Copy-Item -Destination $destinationFolder
+Get-ChildItem -Path $sourceFolder -File | Copy-Item -Destination $destinationFolder -Verbose
 
 # Copy folders and their contents from the source folder to the destination folder
 Get-ChildItem -Path $sourceFolder -Directory | ForEach-Object {
     $destinationPath = Join-Path -Path $destinationFolder -ChildPath $_.Name
-    Copy-Item -Path $_.FullName -Destination $destinationPath -Recurse
+    Copy-Item -Path $_.FullName -Destination $destinationPath -Recurse -Verbose
 }
