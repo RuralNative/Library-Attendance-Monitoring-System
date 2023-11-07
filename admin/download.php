61760303692c70b89a20eea78c5beca1bcaa81da
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-07 11:30:39 +0800

    Provided Documentation for Download PHP Script

--- admin/download.php
+++ admin/download.php
@@ -1,26 +1,37 @@
+<!-- PHP Script providing interactive UI for importing student data to the database through a customized CSV compatible for the system. 
+  -- Uses 'import.php' to process CSV for data storage -->
+
+<!-- Import necessary PHP scripts for session/dependencies -->
 <?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
+
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
+  <!-- Import necessary PHP scripts for display -->
   <?php include 'includes/navbar.php'; ?>
   <?php include 'includes/menubar.php'; ?>
 
-  <!-- Content Wrapper. Contains page content -->
+  <!-- CONTENT WRAPPER (START) -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+
     <section class="content-header">
+      <!-- Main Header --> 
       <h1>
         Students List
       </h1>
+      <!-- Navigation Buttons (Right Corner) -->
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li>Students</li>
+        <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="1index.php">Students</a></li>
         <li class="active">Students List</li>
       </ol>
     </section>
-    <!-- Main content -->
+
+    
     <section class="content">
+      
+      <!-- DO NOT MODIFY (START) -->
       <?php
         if(isset($_SESSION['error'])){
           echo "
@@ -43,49 +54,38 @@
           unset($_SESSION['success']);
         }
       ?>
+      <!-- DO NOT MODIFY (END) -->
+      
+      <!-- CSV Importation Panel -->
       <div class="panel-body">
-                            <div class="table-responsive">
-                               
-			<div class="span6" id="form-login">
-				<form class="form-horizontal well" action="import.php" method="post" name="upload_excel" enctype="multipart/form-data">
-					<fieldset>
-						<legend>Import Bulk Students</legend>
-						<div class="control-group">
-							
-								<label>CSV/Excel File:</label>
-							
-							<div class="controls">
-								<input type="file" multiple name="filename" id="filename" class="input-large">
-							</div>
-						</div>
-						<br/>	
-						<div class="control-group">
-							<div class="controls">
-							<button type="submit" id="submit" name="submit" class="btn btn-primary button-loading" data-loading-text="Loading...">Upload</button>
-							</div>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-
-		
-
-								
-								
-								
-								
-								
-                            </div>
-                            <!-- /.table-responsive -->
-                            
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
+        <div class="table-responsive">
+          <div class="span6" id="form-login">
+				    <form class="form-horizontal well" action="includes/import.php" method="post" name="upload_excel" enctype="multipart/form-data">
+					    <fieldset>
+						    <legend>Bulk Import Students</legend>
+						    <div class="control-group">
+                  <label>CSV/Excel File:</label>
+                  <div class="controls">
+								    <input type="file" multiple name="filename" id="filename" class="input-large">
+							    </div>
+						    </div>
+						    <br/>	
+						    <div class="control-group">
+							    <div class="controls">
+							      <button type="submit" id="submit" name="submit" class="btn btn-primary button-loading" data-loading-text="Loading...">Upload</button>
+							    </div>
+						    </div>
+					    </fieldset>
+				    </form>
+			    </div>
+        </div>
+      </div>
     </section>   
   </div>
-    
+  <!-- CONTENT WRAPPER (END) -->
   <?php include 'includes/footer.php'; ?>
 </div>
+
 <?php include 'includes/scripts.php'; ?>
 
 </body>
