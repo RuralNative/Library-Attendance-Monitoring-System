9a961e681421234ffeb8bc2125465b6997776df6
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-07 13:43:52 +0800

    Modified Scripts PHP for Local Importation
    Modified codebase of Scripts PHP file for using local importation instead of CDN to provide offline compatibility

--- scripts.php
+++ scripts.php
@@ -1,17 +1,16 @@
 <!-- jQuery 3 JS -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="scripts/jquery-3.6.0.min.js"></script>
 <!-- jQuery UI 1.12.1 JS-->
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="scripts/jquery-ui.js"></script>
 <!-- jQuery UI 1.12.1 CSS -->
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="scripts/jquery-ui.css">
 <!-- Bootstrap 4.3.1 JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="scripts/bootstrap.min.js"></script>
 <!-- Bootstrap 4.3.1 CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="scripts/bootstrap.min.css">
 <!-- Font Awesome 5.15.3 CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="scripts/all.min.css">
 <!-- Google Font CCS -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css"/>
+<link rel="stylesheet" href="scripts/css@family=Source+Sans+Pro%3A300,400,600,700,300italic,400italic,600italic.css">
 <!-- Moment JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+<script src="scripts/moment.min.js"></script>
