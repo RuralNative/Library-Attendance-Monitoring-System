1a7d180b0ea7c19af83e974e45fa03e8fc38b83d
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-07 11:13:24 +0800

    Provide Documentation for Import PHP
    Created comments as documentation inside the IMPORT PHP file for future reference

--- admin/includes/import.php
+++ admin/includes/import.php
@@ -1,38 +1,48 @@
 <?php
+// Handles the bulk importation of student information to our student records table of our database
 if (isset($_POST['submit'])) {
+	
+	//Initialize connection to database
 	include 'includes/conn.php';
 	
+	//Show file name/content upon successful upload
 	if (is_uploaded_file($_FILES['filename']['tmp_name'])) {
 		echo "<h1>" . "File ". $_FILES['filename']['name'] ." Uploaded successfully." . "</h1>";
 		echo "<h2>Displaying contents:</h2>";
 		readfile($_FILES['filename']['tmp_name']);
 	}
 
-	//Import uploaded file to Database
+	//PRIMARY code block for CSV Reader initialization and storing fetched data of specific columns to database
 	$handle = fopen($_FILES['filename']['tmp_name'], "r");
-
 	while (($data = fgetcsv($handle, 5000, ",")) !== FALSE) {
 		$reference_number = $data[0];
-		$firstname = $data[1];
-		$mname = $data[2];
-		$lastname = $data[3];
-		$email = $data[4];
-		$phone = $data[5];
-		$residence_status = $data[6];
-		$residence = $data[7];
-		$programme = $data[8];
-		$admission_year = $data[9];
-		$level = $data[10];
+		$fullname = $data[1];
+		$residence = $data[2];
+		$programme = $data[3];
 		
+		//SQL Query for storing data to database
+		$conn -> query(
+			"INSERT INTO students (
+				reference_number, 
+				firstname,
+				residence, 
+				programme, 
+				created_on
+			) VALUES (
+				'$reference_number', 
+				'$fullname', 
+				'$residence', 
+				'$programme', 
+				NOW()
+			)"
+		);
+	}
 
-		$conn->query("INSERT INTO students (reference_number, firstname, mname, lastname, email, phone, residence_status, residence, programme, admission_year, level, created_on) VALUES ('$reference_number', '$firstname', '$mname', '$lastname', '$email', '$phone', '$residence_status', '$residence', '$programme', '$admission_year', '$level', NOW())");
-		
-		}
-
+	// Closes CSV Reader
 	fclose($handle);
 
+	// Show success message and redirects to relevant page
 	echo "<script type='text/javascript'>alert('Successfully imported a CSV file!');</script>";
 	echo "<script>document.location='employee.php'</script>";
 }
-
 ?>
