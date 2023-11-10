7e52798f3bc0b2dd0b469bc214c329ac1dabb10f
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-10 15:11:45 +0800

    Update Header Format/Documentation

--- header.php
+++ header.php
@@ -4,10 +4,8 @@
   	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<title>Library Attendance System</title>
-  	<!-- Tell the browser to be responsive to screen width -->
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<?php include 'scripts.php' ?>
-  	<!-- Theme style -->
   	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
     <link rel="stylesheet" href="dist/css/main_style.css">
 
