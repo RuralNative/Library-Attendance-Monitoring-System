06dab76dfcbe147af4c19a6400a2fa28e6dcecff
John Berlin Leonor <johnberlinleonor@yahoo.com>
2023-11-10 16:06:08 +0800

    Changed PDF Format

--- admin/attendance_generate.php
+++ admin/attendance_generate.php
@@ -36,8 +36,6 @@
 				<td>'.date('M d, Y', strtotime($row['date'])).'</td>
                 <td>'.$row['firstname'].' '.$row['lastname'].'</td>
                 <td>'.$row['empid'].'</td>
-                <td>NULL</td>
-                <td>NULL</td>
                 <td>'.date('h:i A', strtotime($row['time_in'])).'</td>
                 <td>'.date('h:i A', strtotime($row['time_out'])).'</td>
 			</tr>
@@ -56,32 +54,83 @@
 	$from_title = date('M d, Y', strtotime($ex[0]));
 	$to_title = date('M d, Y', strtotime($ex[1]));
 
+    // Set Content for PDF using TCPDF 
 	require_once('../TCPDF-main/tcpdf.php');  
-    $pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+    
+    /*
+        - PDF Generation
+        - Constructor Parameters are as follows:
+        - Portrait/Landscape
+        - Measurement Unit (Inch/Millimeter)
+        - Page Format (A4/Legal/Letter)
+        - Enabling of TCPDF Methods for HTML/CSS
+        - Enabling of TCPDF Methods for JS
+    */ 
+    $pdf = new TCPDF('P', 
+                    PDF_UNIT, 
+                    PDF_PAGE_FORMAT, 
+                    true, 
+                    'UTF-8', 
+                    false);  
+
+    // Set PDF Creator as defined in the TCPDF Library
     $pdf->SetCreator(PDF_CREATOR);  
-    $pdf->SetTitle('SIA Library Attendance: '.$from_title.' - '.$to_title);  
+
+    // Set PDF Title
+    $pdf->SetTitle('CPSU - Moises Padilla Library Attendance Report: '.$from_title.' - '.$to_title);  
+
+    // Set Content Title (Logo, Logo Title, Title, and PDF_HEADER_STRING constant)
     $pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+
+    // Set Header Font (Font Name, Font Style, Font Size)
     $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
-    $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+
+    // Set Footer Font (Font Name, Font Style, Font Size)
+    $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA)); 
+    
+    // Set Default Monospaced Font
     $pdf->SetDefaultMonospacedFont('helvetica');  
+
+    // Set Footer Margin
     $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);  
+
+    // Set Content Margins
     $pdf->SetMargins(PDF_MARGIN_LEFT, '5', PDF_MARGIN_RIGHT);  
+
+    // Determine if Header is to be Printed
     $pdf->setPrintHeader(false);  
-    $pdf->setPrintFooter(false);  
+
+    // Determine if Footer is to be Printed
+    $pdf->setPrintFooter(false); 
+
+    // Set Automated Page Break
     $pdf->SetAutoPageBreak(TRUE, 10);  
+
+    // Set Content Font (Font Name, Font Style, Font Size)
     $pdf->SetFont('helvetica', '', 11);  
+
+    // Adds New Page for Content to be Printed At
     $pdf->AddPage();  
+
+    //Determine Content of PDF
     $content = '';  
     $content .= '
-      	<h2 align="center">SIA Library Attendance Sheet</h2>
-      	<h4 align="center">'.$from_title." - ".$to_title.'</h4>
+      	<h2 align="center" style="color: #096901;">
+            CENTRAL PHILIPPINES STATE UNIVERSITY
+        </h2>
+        <h2 align="center" style="color: #096901;">
+            MOISES PADILLA CAMPUS
+        </h2>
+        <h3 align="center">Campus Library Attendance Report</h3>
+      	<h4 align="center" style="color: #414441;">
+            '.$from_title." - ".$to_title.'
+        </h4>
+
       	<table border="1" cellspacing="0" cellpadding="3">  
            <tr>  
 				  <th width="15%"><b>Date</b></th>
                   <th width="25%"><b>Full Name</b></th>
-                  <th width="15%"><b>Students ID</b></th>
-                  <th width="12%"><b>PURPOSE</b></th>
-                  <th width="10%"><b>GRADE</b></th>
+                  <th width="15%"><b>ID Number</b></th>
                   <th width="12%"><b>Time In</b></th>
                   <th width="12%"><b>Time Out</b></th>
            </tr>  
@@ -90,5 +139,4 @@
     $content .= '</table>';  
     $pdf->writeHTML($content);  
     $pdf->Output('attendance.pdf', 'I');
-
 ?>
